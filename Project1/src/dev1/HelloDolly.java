1787234065803c7d5177893c56f390ad5cec5e4f
Zackery Weeks <zackeryweeks@gmail.com>
2018-07-18 20:36:18 -0400

    changing repo

--- Project1/src/dev1/HelloDolly.java
+++ Project1/src/dev1/HelloDolly.java
@@ -3,8 +3,6 @@ package dev1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		System.out.println("Helow Werld");
-		HellWorld.main(null);
-		System.out.println("WOW");
+		
 	}
 }
